4ee5e6bd5bc9b8ee872208dcb5ec991219ea9260
Jefferson R. Speck <jeffersonspeck@msn.com>
2024-10-10 10:50:16 -0300

    acertos

--- test/models/contract_template_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ContractTemplateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/contract_type_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ContractTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
